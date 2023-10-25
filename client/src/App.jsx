bcfe4f6ad806cb979465e1579612753f60e72a00
Macarena Maria Villagra Velez <macavillagrav@gmail.com>
2023-10-25 19:34:14 -0300

    add appjs and navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from "./views/home/home"
+import ShoppingStore from "./views/shoppingStore/shoppingStore"
+import './App.css'
 import Favoritos from "./components/FavoriteCompras/Favoritos"
-
 
 function App() {
   return (
     <div>
-      <Favoritos/>
+      {/* <Favoritos/> */}
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/store" element={<ShoppingStore />} />
+        <Route path='/favorites' element={<Favoritos/>}></Route>
+        {/* <Route path='/about' element={<About/>}></Route>
+        <Route path='/library' element={<Library/>}></Route>
+        <Route path='/detail/:id' element={<Detail/>}></Route>
+        <Route path="/cart" element={<Cart />} /> */}
+      </Routes>
     </div>
   )
   
--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,21 +1,71 @@
-import { NavLink } from 'react-router-dom';
-import Search from '../search/Search';
-import style from './Navbar.module.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Navbar() {
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState(null);
+
+  const handleVideogameByName = async (e) => {
+
+  };
+
   return (
-    <div>
-      
-      <Search/>
+    <div className={`navbar-wrapper`}>
+      <div className={`navbar-container`}>
+        <div>
+          <span>LOGO</span>
+        </div>
+          <div className='navbar-buttons-container'>
+            <div className='searchbar-cart-container'>
+              {/* searchBar */}
+              <div className='navbar-input d-flex align-items-center position-relative'>
+                <input type="text" placeholder='Enter videogame...' className={`position-relative`} onChange={(e) => setInput(e.target.value)} value={input} style={{ backgroundColor: "white" }}
+                  onKeyDown={(e) => e.key === "Enter" && handleVideogameByName()} />
+                <button className="search-button" onClick={handleVideogameByName}>Buscar</button>
+              </div>
+              
+              {/* CARRITO */}
+              <div className='navbar-icons'>
+                <div>
+                  <img src="/cart.svg" style={{ cursor: "pointer" }} alt="abc" onClick={() => navigate("/")} /> {/* imagen de un carrito */}
+                </div>
 
-      <div className={style.navContent}>
-        <NavLink className={style.homeForm} to= '/'>Inicio</NavLink>
-        <NavLink className={style.homeForm} to= '/store'>Store</NavLink>
-        <NavLink className={style.homeForm} to= '/library'>Library</NavLink>
-        <NavLink className={style.homeForm} to= '/about'>About</NavLink>
+                {/* SIGN UP */}
+                <div>
+                  <button className='navbar-buttons'onClick={() => navigate("/")}>Sign up</button>
+                </div> 
+
+                {/* SIGN IN */}
+                <div>
+                  <button className='navbar-buttons'onClick={() => navigate("/")}>Sign in</button>
+                </div>
+              </div>
+            </div>
+            <div className='searchbar-buttons-container'>
+              {/* INICIO */}
+              <div>
+                <button className='navbar-buttons' onClick={() => navigate("/home")}>Inicio</button>
+              </div>
+
+              {/* TIENDA */}
+              <div>
+                <button className='navbar-buttons' onClick={() => navigate("/store")}>Tienda</button>
+              </div>
+
+              {/* Library */}
+              <div>
+                <button className='navbar-buttons' onClick={() => navigate("/")}>Libary</button>
+              </div>
+
+              {/* ABOUT */}
+              <div>
+                <button className='navbar-buttons' onClick={() => navigate("/")}>About</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
+  )}
 
-export default Navbar;
+export default NavBar;
