8605625247caa72ea04bffe8df304db80973b7b2
luvihu <luvillogas@gmail.com>
2023-10-26 17:51:25 -0500

    cambios adicionales

--- client/src/components/cart/cart.jsx
+++ client/src/components/cart/cart.jsx
@@ -1,8 +1,20 @@
+import style from './cart.module.css';
 
 
 function Cart() {
   return (
-    <div>Cart</div>
+  <div className={style.content}>
+
+    <div className={style.cartContent}>
+      <div className={style.carts}>
+        <h2>Your cart</h2>
+      </div>
+      <div className={style.product}>
+       <h3>Total products</h3>
+       <button>Buy</button>
+      </div>
+    </div>
+  </div>
   )
 }
 
